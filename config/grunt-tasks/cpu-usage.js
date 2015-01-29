31b5cc4e3afcab834ad200e56a0daae53461d691
Jaime Ramírez Castillo <jere@gft.com>
2015-01-29 14:35:27 +0000

    Refractored cpu usage object

--- config/grunt-tasks/cpu-usage.js
+++ config/grunt-tasks/cpu-usage.js
@@ -1,29 +1,47 @@
 'use strict';
 
+var os = require('os');
+
 var CpuUsage = module.exports =  function () {
-    this.previousCicles = 0;
-    this.previousIdleCicles = 0;
-};
+    var
+    previousCycles     = 0,
+    previousIdleCycles = 0,
+    cpus               = [],
+    allCpusCycles      = {};
 
-CpuUsage.prototype.get = function() {
-    var self = this;
-    var cpus = require('os').cpus();
-    var allCpusCicles = {  user : 0,  nice : 0, idle : 0, irq : 0  };
-    var cpuKeys = Object.keys(cpus);
-    cpuKeys.forEach(function(key) {
-      var cpu = cpus[key];
-      allCpusCicles.user += cpu.times.user;
-      allCpusCicles.nice += cpu.times.nice;
-      allCpusCicles.idle += cpu.times.idle;
-      allCpusCicles.irq += cpu.times.irq;
-    });
-    var totalCicles = allCpusCicles.user + allCpusCicles.nice + allCpusCicles.idle + allCpusCicles.irq;
-    var totalIdleCicles = allCpusCicles.idle;
+    this.get = function() {
+        var totalCycles, totalIdleCycles, cpuUsagePercentage;
 
-    var cpuUsagePercentage = 1 - (totalIdleCicles - self.previousIdleCicles ) /
-                                 (totalCicles - self.previousCicles);
+        cpus               = getCpusInfo();
+        allCpusCycles      = {  user : 0,  nice : 0, idle : 0, irq : 0  };
+        Object.keys(cpus)
+            .forEach(addCpuTimesToTotal);
+        totalCycles        = getTotalCycles();
+        totalIdleCycles    = allCpusCycles.idle;
+        cpuUsagePercentage = calculateCpuLoadPercentage(totalCycles, totalIdleCycles);
+        previousCycles     = totalCycles;
+        previousIdleCycles = totalIdleCycles;
+        return cpuUsagePercentage;
+    };
 
-    self.previousCicles = totalCicles;
-    self.previousIdleCicles = totalIdleCicles;
-    return cpuUsagePercentage;
+    function getCpusInfo() {
+        return os.cpus();
+    }
+
+    function getTotalCycles() {
+        return allCpusCycles.user + allCpusCycles.nice + allCpusCycles.idle + allCpusCycles.irq;
+    }
+
+    function calculateCpuLoadPercentage(totalCycles, totalIdleCycles) {
+        return 1 - (totalIdleCycles - previousIdleCycles ) /
+                   (totalCycles - previousCycles);
+    }
+
+    function addCpuTimesToTotal(key) {
+        var cpu = cpus[key];
+        allCpusCycles.user += cpu.times.user;
+        allCpusCycles.nice += cpu.times.nice;
+        allCpusCycles.idle += cpu.times.idle;
+        allCpusCycles.irq += cpu.times.irq;
+    }
 };
