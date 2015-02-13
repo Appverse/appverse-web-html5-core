a2cfb17c6b54ca3de1758e75c3fc281f1f3f5e71
Antoine Charnoz <aecz@gft.com>
2015-02-13 18:37:21 +0100

    - Fixed missing needed modules - Removed unneeded constants

--- test/midway/modules/api-mainSpec.js
+++ test/midway/modules/api-mainSpec.js
@@ -1,21 +1,22 @@
 /*jshint expr:true */
+/*globals should:false */
 'use strict';
 
 describe('MIDWAY | Api REST Module : ', function () {
 
-    beforeEach(function() {
+    beforeEach(function () {
         setupMainTesting();
     });
 
-    describe('when appverse.cache is loaded...', function() {
+    describe('when appverse.cache is loaded...', function () {
 
         beforeEach(function () {
             // create 'fake' security and config modules
-            angular.module('appverse.configuration', []);
+            angular.module('appverse.configuration', []).constant('SECURITY_GENERAL', {});
 
-            module(function($provide) {
+            module(function ($provide) {
                 $provide.value('RESTFactory', {
-                    setCache : sinon.spy(),
+                    setCache: sinon.spy(),
                     enableDefaultContentType: sinon.spy(),
                 });
             });
@@ -23,40 +24,41 @@ describe('MIDWAY | Api REST Module : ', function () {
             module('appverse.cache');
         });
 
-        it('should set an response interceptor to cache modules', inject(function(RESTFactory) {
+        it('should set an response interceptor to cache modules', inject(function (RESTFactory) {
             RESTFactory.setCache.called.should.be.true;
         }));
     });
 
-    describe('when appverse.security is NOT loaded...', function() {
+    describe('when appverse.security is NOT loaded...', function () {
 
         beforeEach(function () {
             angular.module('appverse.configuration', []);
+            module('appverse.cache');
             module('appverse');
         });
 
-        beforeEach(module(function($provide) {
+        beforeEach(module(function ($provide) {
             $provide.constant('SECURITY_GENERAL', {
-                securityEnabled : true, //should be irrelevant
+                securityEnabled: true, //should be irrelevant
             });
 
             $provide.value('Oauth_RequestWrapper', {
-                wrapRequest : sinon.stub().returns(new RestangularMock()),
+                wrapRequest: sinon.stub().returns(new RestangularMock()),
             });
         }));
 
-        it('appverse.rest should NOT wrap requests with oauth', inject(function(Oauth_RequestWrapper) {
+        it('appverse.rest should NOT wrap requests with oauth', inject(function (Oauth_RequestWrapper) {
 
             Oauth_RequestWrapper.wrapRequest.called.should.be.false;
         }));
 
-        it('RESTFactory should set default headers', inject(function(Restangular) {
+        it('RESTFactory should set default headers', inject(function (Restangular) {
             Restangular.setDefaultHeaders.called.should.be.true;
         }));
 
     });
 
-    describe('when appverse.security is loaded...', function() {
+    describe('when appverse.security is loaded...', function () {
 
         beforeEach(function () {
             // create 'fake' security and config modules
@@ -66,45 +68,45 @@ describe('MIDWAY | Api REST Module : ', function () {
             module('appverse');
         });
 
-        describe('and SECURITY_GENERAL.securityEnabled is true...', function() {
+        describe('and SECURITY_GENERAL.securityEnabled is true...', function () {
 
-            beforeEach(module(function($provide) {
+            beforeEach(module(function ($provide) {
                 $provide.constant('SECURITY_GENERAL', {
-                    securityEnabled : true,
+                    securityEnabled: true,
                 });
 
                 $provide.value('Oauth_RequestWrapper', {
-                    wrapRequest : sinon.stub().returns(new RestangularMock()),
+                    wrapRequest: sinon.stub().returns(new RestangularMock()),
                 });
             }));
 
-            it('RESTFactory should wrap requests with oauth', inject(function(Oauth_RequestWrapper) {
+            it('RESTFactory should wrap requests with oauth', inject(function (Oauth_RequestWrapper) {
                 Oauth_RequestWrapper.wrapRequest.called.should.be.true;
             }));
 
-            it('RESTFactory should NOT set default headers', inject(function(Restangular) {
+            it('RESTFactory should NOT set default headers', inject(function (Restangular) {
                 Restangular.setDefaultHeaders.called.should.be.false;
             }));
 
         });
 
-        describe('when SECURITY_GENERAL.securityEnabled is false...', function() {
+        describe('when SECURITY_GENERAL.securityEnabled is false...', function () {
 
-            beforeEach(module(function($provide) {
+            beforeEach(module(function ($provide) {
                 $provide.constant('SECURITY_GENERAL', {
-                    securityEnabled : false,
+                    securityEnabled: false,
                 });
 
                 $provide.value('Oauth_RequestWrapper', {
-                    wrapRequest : sinon.stub().returns(new RestangularMock()),
+                    wrapRequest: sinon.stub().returns(new RestangularMock()),
                 });
             }));
 
-            it('appverse.rest should NOT wrap requests with oauth', inject(function(Oauth_RequestWrapper) {
+            it('appverse.rest should NOT wrap requests with oauth', inject(function (Oauth_RequestWrapper) {
                 Oauth_RequestWrapper.wrapRequest.called.should.be.false;
             }));
 
-            it('RESTFactory should set default headers', inject(function(Restangular) {
+            it('RESTFactory should set default headers', inject(function (Restangular) {
                 Restangular.setDefaultHeaders.called.should.be.true;
             }));
 
@@ -117,12 +119,12 @@ describe('MIDWAY | Api REST Module : ', function () {
 
 
 
-describe('REST_CONFIG.MockBackend is enabled...', function() {
+describe('REST_CONFIG.MockBackend is enabled...', function () {
 
-    beforeEach(module(function($provide) {
+    beforeEach(module(function ($provide) {
         $provide.constant('REST_CONFIG', {
-            ElementTransformer : [],
-            MockBackend : true
+            ElementTransformer: [],
+            MockBackend: true
         });
         $provide.constant('LOGGING_CONFIG', {});
         $provide.constant('PROJECT_DATA', {});
@@ -131,31 +133,33 @@ describe('REST_CONFIG.MockBackend is enabled...', function() {
         $provide.constant('I18N_CONFIG', {});
         $provide.constant('SERVERPUSH_CONFIG', {});
         $provide.constant('SECURITY_GENERAL', {});
-        $provide.provider('ConfigLoader', function() {
+        $provide.provider('ConfigLoader', function () {
             this.setDetection = sinon.spy();
             this.$get = sinon.spy();
-        } );
-        $provide.provider('Oauth_RequestWrapper', function() {
+        });
+        $provide.provider('Oauth_RequestWrapper', function () {
             this.$get = sinon.spy();
-        } );
+        });
         $provide.value('$httpBackend', {});
     }));
 
+    beforeEach(module('appverse.detection'));
+    beforeEach(module('appverse.logging'));
     beforeEach(module('appverse'));
 
-    it('$httpBackend should be mocked', inject(function($httpBackend) {
+    it('$httpBackend should be mocked', inject(function ($httpBackend) {
         should.exist($httpBackend.whenGET);
     }));
 
 });
 
 
-describe('REST_CONFIG.MockBackend is DISABLED...', function() {
+describe('REST_CONFIG.MockBackend is DISABLED...', function () {
 
-    beforeEach(module(function($provide) {
+    beforeEach(module(function ($provide) {
         $provide.constant('REST_CONFIG', {
-            ElementTransformer : [],
-            MockBackend : false
+            ElementTransformer: [],
+            MockBackend: false
         });
         $provide.constant('LOGGING_CONFIG', {});
         $provide.constant('PROJECT_DATA', {});
@@ -164,57 +168,52 @@ describe('REST_CONFIG.MockBackend is DISABLED...', function() {
         $provide.constant('I18N_CONFIG', {});
         $provide.constant('SERVERPUSH_CONFIG', {});
         $provide.constant('SECURITY_GENERAL', {});
-        $provide.provider('ConfigLoader', function() {
+        $provide.provider('ConfigLoader', function () {
             this.setDetection = sinon.spy();
             this.$get = sinon.spy();
-        } );
-        $provide.provider('Oauth_RequestWrapper', function() {
+        });
+        $provide.provider('Oauth_RequestWrapper', function () {
             this.$get = sinon.spy();
-        } );
+        });
         $provide.value('$httpBackend', {});
     }));
 
+    beforeEach(module('appverse.detection'));
+    beforeEach(module('appverse.logging'));
     beforeEach(module('appverse'));
 
-    it('$httpBackend should be mocked', inject(function($httpBackend) {
+    it('$httpBackend should be mocked', inject(function ($httpBackend) {
         should.not.exist($httpBackend.whenGET);
     }));
 
 });
 
-
 function setupMainTesting() {
 
     // First load the module
     module('appverse.rest');
+    module('appverse.detection');
+    module('appverse.logging');
 
     // then Override real services with mocks
-    module(function($provide) {
+    module(function ($provide) {
 
         $provide.service('Restangular', RestangularMock);
 
-        $provide.provider('ConfigLoader', function() {
+        $provide.provider('ConfigLoader', function () {
             this.setDetection = sinon.spy();
-            this.$get = function () {return this;};
+            this.$get = function () {
+                return this;
+            };
         });
-
-        $provide.constant('PROJECT_DATA', {});
-
-        $provide.constant('DETECTION_CONFIG', {});
 
         $provide.constant('LOGGING_CONFIG', {});
 
         $provide.constant('CACHE_CONFIG', {});
 
-        $provide.constant('SECURITY_GENERAL', {});
-
         $provide.constant('REST_CONFIG', {
-            ElementTransformer : []
+            ElementTransformer: []
         });
-
-        $provide.constant('I18N_CONFIG', {});
-
-        $provide.constant('SERVERPUSH_CONFIG', {});
     });
 }
 
