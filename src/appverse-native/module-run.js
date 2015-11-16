ebf781b675aed4d1d64d002f83aeb2e5d7ddf1a5
Antoine Charnoz <antoine.charnoz@gft.com>
2015-11-16 09:25:04 +0100

    Moved Appverse.is to detection module Added auto HTTP redirect to appverse native

--- src/appverse-native/module-run.js
+++ src/appverse-native/module-run.js
@@ -1,199 +1,119 @@
+/*globals callbackGeoStart:false */
 (function () {
     'use strict';
 
-    angular.module('appverse.native').factory('AppverseNative', AppverseNative);
+    angular.module('appverse.native')
 
     /**
      * @ngdoc service
      * @name AppverseNative
      * @module appverse.native
      * @description This module provides basic quick standard access to a Native functions
-     *
      */
+    .factory('AppverseNative',
+        function ($log, $q, Detection, $window) {
+            ////////////////////////////////////////////////////////////////////////////////////
+            // ADVICES ABOUT PROMISES
+            //
+            // 1-PROMISES
+            // All Restangular requests return a Promise. Angular's templates
+            // are able to handle Promises and they're able to show the promise
+            // result in the HTML. So, if the promise isn't yet solved, it shows
+            // nothing and once we get the data from the server, it's shown in the template.
+            // If what we want to do is to edit the object you get and then do a put, in
+            // that case, we cannot work with the promise, as we need to change values.
+            // If that's the case, we need to assign the result of the promise to a $scope variable.
+            ////////////////////////////////////////////////////////////////////////////////////
 
-    function AppverseNative($log, $q, Detection, $window) {
-        ////////////////////////////////////////////////////////////////////////////////////
-        // ADVICES ABOUT PROMISES
-        //
-        // 1-PROMISES
-        // All Restangular requests return a Promise. Angular's templates
-        // are able to handle Promises and they're able to show the promise
-        // result in the HTML. So, if the promise isn't yet solved, it shows
-        // nothing and once we get the data from the server, it's shown in the template.
-        // If what we want to do is to edit the object you get and then do a put, in
-        // that case, we cannot work with the promise, as we need to change values.
-        // If that's the case, we need to assign the result of the promise to a $scope variable.
-        ////////////////////////////////////////////////////////////////////////////////////
+            var factory, deferredGeo, deferredNetwork, geoTryNumber;
+            factory = deferredGeo = deferredNetwork = {};
+            geoTryNumber = 0;
 
-        var factory, deferredGeo, deferredNetwork, geoTryNumber;
-        factory = deferredGeo = deferredNetwork = {};
-        geoTryNumber = 0;
+            $window.callbackGeo = function (result) {
+                if (result) {
+                    Appverse.Geo.StartUpdatingLocation('callbackGeoStart', 'callbackGeoStart');
+                } else {
+                    deferredGeo.reject(result);
+                }
+            };
 
-        var promises = [];
+            $window.callbackGeoStart = function (result) {
+                if (result) {
+                    Appverse.Geo.GetCoordinates('callbackGeoCoordinates', 'callbackGeoCoordinates');
+                } else {
+                    deferredGeo.reject(result);
+                }
+            };
 
-        $window.callbackGeo = function (result) {
-            if (result) {
-                Appverse.Geo.StartUpdatingLocation('callbackGeoStart', 'callbackGeoStart');
-            } else {
-                deferredGeo.reject(result);
-            }
-        };
-
-        $window.callbackGeoStart = function (result) {
-            if (result) {
-                Appverse.Geo.GetCoordinates('callbackGeoCoordinates', 'callbackGeoCoordinates');
-            } else {
-                deferredGeo.reject(result);
-            }
-        };
-
-        $window.callbackGeoCoordinates = function (coordinates) {
-            geoTryNumber++;
-            if (coordinates.XCoordinate != 0 && coordinates.YCoordinate != 0 && coordinates.XDoP < 500 && coordinates.YDoP < 500) {
-                deferredGeo.resolve(coordinates);
-                Appverse.Geo.StopUpdatingLocation('callbackGeoStop', 'callbackGeoStop');
-            } else {
-                if (geoTryNumber > 10) {
-                    deferredGeo.reject(coordinates);
+            $window.callbackGeoCoordinates = function (coordinates) {
+                geoTryNumber++;
+                if (coordinates.XCoordinate !== 0 && coordinates.YCoordinate !== 0 &&
+                    coordinates.XDoP < 500 && coordinates.YDoP < 500) {
+                    deferredGeo.resolve(coordinates);
                     Appverse.Geo.StopUpdatingLocation('callbackGeoStop', 'callbackGeoStop');
                 } else {
-                    setTimeout(function () {
-                        callbackGeoStart(true);
-                    }, 2000);
+                    if (geoTryNumber > 10) {
+                        deferredGeo.reject(coordinates);
+                        Appverse.Geo.StopUpdatingLocation('callbackGeoStop', 'callbackGeoStop');
+                    } else {
+                        setTimeout(function () {
+                            callbackGeoStart(true);
+                        }, 2000);
+                    }
                 }
-            }
-        };
-
-        $window.StopUpdatingLocation = function (result) {};
-
-        $window.callbackNetwork = function (result) {
-            if (result) {
-                deferredNetwork.resolve(result);
-            } else {
-                deferredNetwork.reject(result);
-            }
-        };
-
-
-
-        factory.getCoordinates = function () {
-            deferredGeo = $q.defer();
-            if (Detection.hasAppverseMobile()) {
-                Appverse.Geo.IsGPSEnabled('callbackGeo', 'callbackGeo');
-            } else {
-                if (navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition(showPosition);
-                } else {
-                    return deferredGeo.reject(false);
-                }
-            }
-            return deferredGeo.promise;
-        };
-
-        function showPosition(position) {
-            var coordinates = {
-                "XCoordinate": position.coords.latitude,
-                "YCoordinate": position.coords.longitude,
-                "ZCoordinate": 0,
-                "XDoP": position.coords.accuracy,
-                "YDoP": position.coords.accuracy
             };
-            deferredGeo.resolve(coordinates);
-        }
 
+            $window.StopUpdatingLocation = function () {};
 
-        factory.isNetworkReachable = function () {
-            deferredNetwork = $q.defer();
-
-            if (Detection.hasAppverseMobile()) {
-                Appverse.Net.IsNetworkReachable("www.google.com", 'callbackNetwork', 'callbackNetwork');
-            } else {
-                if (Detection.isOnline) {
-                    deferredNetwork.resolve(true);
+            $window.callbackNetwork = function (result) {
+                if (result) {
+                    deferredNetwork.resolve(result);
                 } else {
-                    deferredNetwork.reject(false);
+                    deferredNetwork.reject(result);
                 }
+            };
+
+            factory.getCoordinates = function () {
+                deferredGeo = $q.defer();
+                if (Detection.hasAppverseMobile()) {
+                    Appverse.Geo.IsGPSEnabled('callbackGeo', 'callbackGeo');
+                } else {
+                    if (navigator.geolocation) {
+                        navigator.geolocation.getCurrentPosition(showPosition);
+                    } else {
+                        return deferredGeo.reject(false);
+                    }
+                }
+                return deferredGeo.promise;
+            };
+
+            function showPosition(position) {
+                var coordinates = {
+                    "XCoordinate": position.coords.latitude,
+                    "YCoordinate": position.coords.longitude,
+                    "ZCoordinate": 0,
+                    "XDoP": position.coords.accuracy,
+                    "YDoP": position.coords.accuracy
+                };
+                deferredGeo.resolve(coordinates);
             }
-            return deferredNetwork.promise;
-        };
-
-        factory.getBrowserInformation = function () {
-            if (Detection.hasAppverseMobile()) {
-                return {
-                    isMobileBrowser: !Appverse.is.Desktop,
-                    isIphone: Appverse.is.iPhone,
-                    isWindowsPhone: Appverse.is.Windows,
-                    isWindows: Appverse.is.Windows,
-                    isBlackberry: Appverse.is.Blackberry,
-                    isAndroid: Appverse.is.Android,
-                    isiOS: Appverse.is.iOS,
-                    isTablet: Appverse.is.Tablet,
-                    isPhone: !Appverse.is.Desktop,
-                    isIpod: Appverse.is.iPod,
-                    isIpad: Appverse.is.iPad,
-                    isLinux: Appverse.is.Linux,
-                    isMac: Appverse.is.Mac
-                };
-            } else {
-                var iOS, iphone, ipod, ipad, android, mac, windows, linux, blackberry, tablet, windowsPhone;
-                iOS = iphone = ipod = ipad = android = mac = windows = linux = blackberry = tablet = windowsPhone = false;
-                if (navigator.userAgent.match(/iPhone/i)) {
-                    iOS = true;
-                    iphone = true;
-                }
-                if (navigator.userAgent.match(/iPod/i)) {
-                    iOS = true;
-                    ipod = true;
-                }
-                if (navigator.userAgent.match(/iPad/i)) {
-                    iOS = true;
-                    ipad = true;
-                }
-                if (navigator.userAgent.indexOf("android") > -1) {
-                    android = true;
-                }
-                if (navigator.platform.toUpperCase().indexOf('MAC') >= 0) {
-                    mac = true;
-                }
-                if (navigator.appVersion.indexOf("Win") != -1) {
-                    windows = true;
-                };
-                if (navigator.appVersion.indexOf("Linux") != -1) {
-                    linux = true;
-                };
-                if (navigator.userAgent.toLowerCase().indexOf("blackberry") >= 0) {
-                    blackberry = true;
-                }
-                if ((/ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(navigator.userAgent.toLowerCase()))) {
-                    tablet = true;
-                }
-                if (navigator.userAgent.match(/Windows Phone/i) || navigator.userAgent.match(/iemobile/i)) {
-                    windowsPhone = true;
-                }
 
 
-                return {
-                    isMobileBrowser: Detection.isMobileBrowser(),
-                    isIphone: iphone,
-                    isWindowsPhone: windowsPhone,
-                    isWindows: windows,
-                    isBlackberry: blackberry,
-                    isAndroid: android,
-                    isiOS: iOS,
-                    isTablet: tablet,
-                    isPhone: Detection.isMobileBrowser(),
-                    isIpod: ipod,
-                    isIpad: ipad,
-                    isLinux: linux,
-                    isMac: mac
-                };
-            }
-        };
+            factory.isNetworkReachable = function () {
+                deferredNetwork = $q.defer();
 
+                if (Detection.hasAppverseMobile()) {
+                    Appverse.Net.IsNetworkReachable("www.google.com", 'callbackNetwork', 'callbackNetwork');
+                } else {
+                    if (Detection.isOnline) {
+                        deferredNetwork.resolve(true);
+                    } else {
+                        deferredNetwork.reject(false);
+                    }
+                }
+                return deferredNetwork.promise;
+            };
 
-        return factory;
-
-    }
-    AppverseNative.$inject = ["$log", "$q", "Detection", "$window"];
-
+            return factory;
+        });
 })();
--- src/appverse-native/module.js
+++ src/appverse-native/module.js
@@ -1,3 +1,4 @@
+/*globals AppverseEmulator, get_params:false, unescape:false */
 (function () {
     'use strict';
 
@@ -10,7 +11,274 @@
      *
      * @requires appverse.detection
      */
-    angular.module('appverse.native', ['appverse.detection']);
+    angular.module('appverse.native', ['appverse.detection'])
+
+    .config(
+        function ($httpProvider, DetectionProvider, REST_CONFIG) {
+
+            function createMap() {
+                return Object.create(null);
+            }
+
+            var trim = function (value) {
+                return angular.isString(value) ? value.trim() : value;
+            };
+
+            function parseHeaders(headers) {
+                var parsed = createMap(),
+                    key, val, i;
+
+                if (!headers) {
+                    return parsed;
+                }
+
+                angular.forEach(headers.split('\n'), function (line) {
+                    i = line.indexOf(':');
+                    key = angular.lowercase(trim(line.substr(0, i)));
+                    val = trim(line.substr(i + 1));
+
+                    if (key) {
+                        parsed[key] = parsed[key] ? parsed[key] + ', ' + val : val;
+                    }
+                });
+
+                return parsed;
+            }
+
+            function headersGetter(headers) {
+                var headersObj = angular.isObject(headers) ? headers : undefined;
+
+                return function (name) {
+                    if (!headersObj) {
+                        headersObj = parseHeaders(headers);
+                    }
+
+                    if (name) {
+                        var value = headersObj[angular.lowercase(name)];
+                        if (value === void 0) {
+                            value = null;
+                        }
+                        return value;
+                    }
+
+                    return headersObj;
+                };
+            }
+
+            window.$httpCounter = -1;
+
+            window.$httpCallback = function (result, id) {
+
+                console.log('$httpCallback', id);
+                console.log('$httpCallback', result);
+
+                var response = {};
+                response.config = window['$httpConfig' + id];
+                delete window['$httpConfig' + id];
+
+                if (result !== null) {
+                    var headers = '';
+                    angular.forEach(result.Headers, function (header) {
+                        headers += header.Name + ':' + header.Value + '\n';
+                    });
+
+                    response.headers = headersGetter(headers);
+
+                    if (result.Content.length > 0) {
+                        response.data = JSON.parse(result.Content);
+                    }
+                    response.status = 200;
+                    response.statusText = "OK";
+
+                    if (response.config.cache && $httpProvider.defaults.cache && response.config.method === 'GET') {
+                        $httpProvider.defaults.cache.put(response.config.url, [response.status, response.data, response.headers(), response.statusText]);
+                    }
+
+                    window['$httpPromise' + id].resolve(response);
+                } else {
+                    response.status = 404;
+                    response.statusText = "Error";
+
+                    window['$httpPromise' + id].reject(response);
+                }
+                delete window['$httpPromise' + id];
+            };
+
+            var setIoService = function (config) {
+                var ioService = {};
+                ioService.Endpoint = {};
+                ioService.Endpoint.Path = config.url.split(REST_CONFIG.BaseUrl)[1];
+
+                if (!REST_CONFIG.HostList[config.ind]) {
+                    console.error("Host not found in Host list: ", REST_CONFIG.HostList);
+                } else {
+                    ioService.Endpoint.Host = REST_CONFIG.HostList[config.ind].Host;
+                    ioService.Endpoint.Port = REST_CONFIG.HostList[config.ind].Port;
+                }
+
+                if (REST_CONFIG.ServerMode === 'MockServer') {
+                    ioService.Endpoint.ProxyUrl = REST_CONFIG.MockServer.URL + ':' + REST_CONFIG.MockServer.Port;
+                } else {
+                    ioService.Endpoint.ProxyUrl = "";
+                }
+
+                return ioService;
+            };
+
+            var setRequestObject = function (config) {
+                var request = {};
+                request.Headers = [];
+
+                //Pending Appverse Mobile format
+                //            angular.forEach(config.headers, function(value, header) {
+                //                request.Headers.push({
+                //                    Name: header,
+                //                    Value: value
+                //                })
+                //            });
+
+                request.Content = config.data;
+                request.ContentType = "application/json";
+                request.Session = {};
+                request.Method = config.method;
+                request.StopAutoRedirect = false;
+
+                return request;
+            };
+
+            $httpProvider.interceptors.push(function ($q) {
+                return {
+                    request: function (config) {
+
+                        if (config.url.indexOf(REST_CONFIG.BaseUrl) !== 0 || !DetectionProvider.hasAppverseMobile()) {
+                            console.log('request fallthrough', config);
+                            return config;
+                        }
+
+                        if (config.cache && config.method === 'GET' && $httpProvider.defaults.cache && $httpProvider.defaults.cache.get(config.url)) {
+                            console.log('request cached', config);
+                            return config;
+                        }
+
+                        console.log('request override', config);
+
+                        window.$httpCounter++;
+                        window['$httpPromise' + window.$httpCounter] = $q.defer();
+                        window['$httpConfig' + window.$httpCounter] = angular.copy(config);
+
+                        var methodName = "InvokeService";
+                        var callBackFuncName = "$httpCallback";
+                        var callbackId = window.$httpCounter;
+
+                        if (!config.ind) {
+                            config.ind = 0;
+                        }
+
+                        var params = get_params([setRequestObject(config), setIoService(config)]);
+
+                        var legacyPath = Appverse.SERVICE_URI + Appverse.IO.serviceName + "/" + methodName;
+                        var newPath = Appverse.APPVERSE_SERVICE_URI + Appverse.IO.serviceName + "/" + methodName; // new path for Appverse 5.0 (applied when possible)
+
+                        var path = legacyPath; // by default, use legacy path
+
+                        if (Appverse.is.iOS) {
+                            path = newPath; // we use the new path for all iOS devices
+                        }
+
+                        var reqData = "";
+
+                        if (callBackFuncName !== null) {
+                            reqData = reqData + "callback=" + callBackFuncName;
+                        } else {
+                            reqData = reqData + "callback=NULL";
+                        }
+                        if (callbackId !== null) {
+                            reqData = reqData + "&callbackid=" + callbackId;
+                        } else {
+                            reqData = reqData + "&callbackid=callbackid";
+                            callbackId = "callbackId";
+                        }
+
+                        if (params !== null) {
+                            //reqData = reqData + "&json=" + unescape(params);
+                            if (Appverse.unescapeNextRequestData) {
+                                reqData = reqData + "&json=" + unescape(params);
+                            } else {
+                                reqData = reqData + "&json=" + params; // we don't unscape parameters if configured
+                                Appverse.unescapeNextRequestData = true; // returning to default configuration value
+                            }
+                        }
+
+                        if (window.webkit) {
+                            // using new WKWebView message handlers, if available (iOS 8)
+                            window.webkit.messageHandlers.service.postMessage({
+                                uri: path,
+                                query: reqData
+                            });
+                            return;
+                        }
+
+                        if (window.appverseJSBridge) { // only available for 4.2+ Android devices
+                            path = newPath;
+                            window.appverseJSBridge.postMessage(path, reqData);
+                            return;
+                        }
+
+                        if (window.external && window.external.notify) { // using external post notifications for Windows Phone
+                            var t = {
+                                uri: newPath,
+                                query: reqData
+                            };
+                            window.external.notify(JSON.stringify(t));
+                            return;
+                        }
+
+                        if (AppverseEmulator.eventListenersRegistered && AppverseEmulator.eventListenersRegistered.indexOf(methodName) >= 0) {
+                            AppverseEmulator.queuedListenerMessagesCount++;
+                            console.log("Appverse Emulator - queue listener result for methodName: " + AppverseEmulator.normalizeListenerCallingName(methodName));
+
+                            (function (smn) {
+                                setTimeout(function () {
+                                    AppverseEmulator.appverseListenerPollingTimerFunc(smn);
+                                }, AppverseEmulator.pollingInterval);
+                            })(Appverse.IO.serviceName + "#" + AppverseEmulator.normalizeListenerCallingName(methodName));
+                        }
+
+                        if (Appverse.executingInEmulator) {
+                            if (callBackFuncName !== null) {
+                                AppverseEmulator.queuedCallbackMessagesCount++;
+                                console.log("Appverse Emulator - queue callback result for methodName: " + methodName);
+                                (function (c, ci) {
+                                    setTimeout(function () {
+                                        AppverseEmulator.appverseCallbackPollingTimerFunc(c, ci);
+                                    }, AppverseEmulator.pollingInterval);
+                                })(callBackFuncName, callbackId);
+                            }
+                        }
+
+                        delete config.headers.Accept;
+                        config.headers['Content-Type'] = 'application/json;charset=UTF-8';
+                        config.method = 'POST';
+                        config.url = path;
+                        config.data = reqData;
+
+                        return config;
+                    },
+                    response: function (response) {
+
+                        if (!response.config.url || response.config.url.indexOf(Appverse.SERVICE_URI + Appverse.IO.serviceName + "/InvokeService") !== 0) {
+                            return response;
+                        }
+
+                        console.log('response override', response);
+
+                        var counter = response.config.data.split('callbackid=')[1].split('&')[0];
+                        return window['$httpPromise' + counter].promise;
+                    }
+                };
+            });
+
+        });
 
 
 })();
