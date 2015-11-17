4a1b21889131e666999551502fad9132f50ac889
Antoine Charnoz <antoine.charnoz@gft.com>
2015-11-17 13:12:51 +0100

    Removed commented code Fixed logging unit tests Added native unit tests

--- test/unit/modules/api-utilsSpec.js
+++ test/unit/modules/api-utilsSpec.js
@@ -1,4 +1,4 @@
-/*jshint expr:true */
+/*jshint expr:true, node:true */
 "use strict";
 
 describe('Unit: Testing moduleSeeker ', function () {
@@ -7,21 +7,21 @@ describe('Unit: Testing moduleSeeker ', function () {
 
     beforeEach('register the module', module('appverse.utils'));
 
-    beforeEach('inject ModuleSeeker', inject(function(ModuleSeeker) {
+    beforeEach('inject ModuleSeeker', inject(function (ModuleSeeker) {
         moduleSeeker = ModuleSeeker;
     }));
 
-    describe('when a module does not exist...', function() {
+    describe('when a module does not exist...', function () {
 
-        it('should return false', function() {
+        it('should return false', function () {
             moduleSeeker.exists('nonExistingModule').should.be.false;
         });
 
     });
 
-    describe('when a module exists...', function() {
+    describe('when a module exists...', function () {
 
-        it('should return true', function() {
+        it('should return true', function () {
             moduleSeeker.exists('appverse.utils').should.be.true;
         });
 
@@ -32,22 +32,22 @@ describe('Unit: Testing moduleSeeker ', function () {
 
 describe('Unit: Testing Relative URL generator ', function () {
 
-    describe('when adding a base URL to a relative URLS...', function() {
+    describe('when adding a base URL to a relative URLS...', function () {
 
         var BaseUrlSetter;
 
         beforeEach('register the module', module('appverse.utils'));
 
-        beforeEach('inject BaseUrlSetter', inject(function(_BaseUrlSetter_) {
+        beforeEach('inject BaseUrlSetter', inject(function (_BaseUrlSetter_) {
             BaseUrlSetter = _BaseUrlSetter_;
         }));
 
-        describe('when base URL does not have trailing slash', function() {
+        describe('when base URL does not have trailing slash', function () {
 
             var url = 'mypath.html',
-            baseUrl = '/a/base/url';
+                baseUrl = '/a/base/url';
 
-            it('should return the complete URL as the concatenation of both URLS', function() {
+            it('should return the complete URL as the concatenation of both URLS', function () {
 
                 BaseUrlSetter
                     .setBasePath(baseUrl)
@@ -57,12 +57,12 @@ describe('Unit: Testing Relative URL generator ', function () {
 
         });
 
-        describe('when base URL has trailing slash', function() {
+        describe('when base URL has trailing slash', function () {
 
             var url = 'mypath.html',
-            baseUrl = '/a/base/url/';
+                baseUrl = '/a/base/url/';
 
-            it('should return the complete URL as the concatenation of both URLS', function() {
+            it('should return the complete URL as the concatenation of both URLS', function () {
 
                 BaseUrlSetter
                     .setBasePath(baseUrl)
@@ -72,12 +72,12 @@ describe('Unit: Testing Relative URL generator ', function () {
 
         });
 
-        describe('when base URL has trailing slash and other URL too', function() {
+        describe('when base URL has trailing slash and other URL too', function () {
 
             var url = '/mypath.html',
-            baseUrl = '/a/base/url/';
+                baseUrl = '/a/base/url/';
 
-            it('should return the complete URL as the concatenation of both URLS', function() {
+            it('should return the complete URL as the concatenation of both URLS', function () {
 
                 BaseUrlSetter
                     .setBasePath(baseUrl)
@@ -87,12 +87,12 @@ describe('Unit: Testing Relative URL generator ', function () {
 
         });
 
-        describe('when URL have extra  spaces', function() {
+        describe('when URL have extra  spaces', function () {
 
             var url = ' /mypath.html',
-            baseUrl = '/a/base/url/  ';
+                baseUrl = '/a/base/url/  ';
 
-            it('should return the complete URL as the concatenation of both URLS', function() {
+            it('should return the complete URL as the concatenation of both URLS', function () {
 
                 BaseUrlSetter
                     .setBasePath(baseUrl)
