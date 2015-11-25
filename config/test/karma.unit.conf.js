032b4f77e813386735d3126f5abf058800a1bbc9
Antoine Charnoz <antoine.charnoz@gft.com>
2015-11-25 18:55:55 +0100

    Updated SockJS version Updated jshint, phantomjs-launcher Removed test and analysis from dist task Removed websocket server task

--- config/test/karma.unit.conf.js
+++ config/test/karma.unit.conf.js
@@ -10,7 +10,27 @@ module.exports = function (config) {
 
         frameworks: ['mocha', 'chai', 'sinon'],
 
-        browsers: ['PhantomJS'],
+        browsers: ['PhantomJS_custom'],
+
+        // you can define custom flags
+        customLaunchers: {
+            'PhantomJS_custom': {
+                base: 'PhantomJS2',
+                options: {
+                    windowName: 'my-window',
+                    settings: {
+                        webSecurityEnabled: false
+                    }
+                },
+                flags: ['--load-images=true', '--local-to-remote-url-access=true'],
+                debug: true
+            }
+        },
+
+        phantomjsLauncher: {
+            // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
+            exitOnResourceError: true
+        },
 
         reporters: ['progress', 'coverage', 'notify', 'junit'],
 
@@ -24,13 +44,11 @@ module.exports = function (config) {
         coverageReporter: {
             // specify a common output directory
             dir: 'reports/coverage',
-            reporters: [
-                {
-                    type: 'lcov'
-                }, {
-                    type: 'clover'
-                }
-            ]
+            reporters: [{
+                type: 'lcov'
+            }, {
+                type: 'clover'
+            }]
         },
 
         junitReporter: {
@@ -43,6 +61,8 @@ module.exports = function (config) {
             'bower_components/angular-translate/angular-translate.min.js',
             'bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js',
             'bower_components/angular-dynamic-locale/dist/tmhDynamicLocale.js',
+            'bower_components/sockjs-client/dist/sockjs.js',
+            'bower_components/stomp-websocket/lib/stomp.min.js',
 
             'src/appverse-*/**/module.js',
             // Detection providers need to be loaded in this order
