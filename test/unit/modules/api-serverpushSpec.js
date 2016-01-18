c0c9315f5a3249dd092acfbf8c2d6bfd8ac00ecb
Antoine Charnoz <antoine.charnoz@gft.com>
2016-01-18 16:41:40 +0100

    Increased timeout for websocket tests

--- test/unit/modules/api-serverpushSpec.js
+++ test/unit/modules/api-serverpushSpec.js
@@ -2,39 +2,41 @@
 
 "use strict";
 
-describe('Unit: Testing appverse.serverPush module', function() {
+describe('Unit: Testing appverse.serverPush module', function () {
 
     var WebSocketFactory, $httpBackend;
 
+    this.timeout(10000);
+
     beforeEach(module('appverse.serverPush'));
 
-    beforeEach(inject(function(_WebSocketFactory_, _$httpBackend_) {
+    beforeEach(inject(function (_WebSocketFactory_, _$httpBackend_) {
         WebSocketFactory = _WebSocketFactory_;
         $httpBackend = _$httpBackend_;
 
         expect(WebSocketFactory).to.be.an.object;
     }));
 
-    afterEach(function() {
+    afterEach(function () {
         $httpBackend.verifyNoOutstandingExpectation();
         $httpBackend.verifyNoOutstandingRequest();
     });
 
-    it('should contain a SocketFactory factory', inject(function(SocketFactory) {
+    it('should contain a SocketFactory factory', inject(function (SocketFactory) {
         expect(SocketFactory).to.be.an.object;
     }));
 
-    it('should open a WebSocket connection', function(done) {
+    it('should open a WebSocket connection', function (done) {
         WebSocketFactory.open(null, null,
-            function() {
+            function () {
                 expect(WebSocketFactory.ws).to.be.defined;
                 WebSocketFactory.connect('user', 'password');
                 done();
             });
     });
 
-    it('should connect to a WebSocket connection ', function(done) {
-        WebSocketFactory.connect('user', 'password', function() {
+    it('should connect to a WebSocket connection ', function (done) {
+        WebSocketFactory.connect('user', 'password', function () {
             expect(WebSocketFactory.client).to.be.defined;
         });
         done();
